ae8d5b85657d7e008f209acd1ad086505d9c3437
David Gitman <1743448+dgitman@users.noreply.github.com>
2026-08-16 12:06:45 -0400

    Fix Speed Insights integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,8 +17,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
-      <SpeedInsights />
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
       <GoogleAnalytics gaId="G-JLQCKDH98X" />
     </html>
   );
